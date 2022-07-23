d2b261828faecd3d29d8beb6ed9d96a4e65ddd7e
Haris-Ali <haris.ali@vizteck.com>
2022-07-23 14:53:44 +0500

    api calls

--- src/components/Activity/Activity.jsx
+++ src/components/Activity/Activity.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { getRequest } from '../../apis'
 import { ENDPOINTS } from '../../apis/endpoints'
-import { BsFillTelephoneInboundFill, BsFillTelephoneOutboundFill } from "react-icons/bs";
+import { BsFillTelephoneInboundFill, BsFillTelephoneOutboundFill, BsArchiveFill, BsFillEyeFill } from "react-icons/bs"
+import { BiReset } from 'react-icons/bi'
 import moment from 'moment'
 import './Activity.css'
+import { useNavigate } from 'react-router-dom'
 
 const Activity = () => {
     const [activityData, setActivityData] = useState([])
+
+    const navigate = useNavigate()
     
     useEffect(() => {
         getActivityList()
@@ -22,11 +26,43 @@ const Activity = () => {
             console.log(error)
         }
     }
-    
+
+    const archiveAllCalls = () => {
+        try {
+        
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const resetCalls = async () => {
+        try {
+            const URL = ENDPOINTS.resetCalls
+            const { data, status } = await getRequest(URL)
+            // console.log(data, status)
+            if (status === 200) getActivityList() // if reset call successful, fetch logs again
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const archiveCall = async (callID) => {
+        
+    }    
 
     return (
         <div>
             <h4 className='heading'>Activity Logs</h4>
+            <div className='actionBtnBlock'>
+                <button className='archiveBtn actionBtn' onClick={() => archiveAllCalls()}>
+                    <BsArchiveFill />
+                    Archive All Calls
+                </button>
+                <button className='resetBtn actionBtn' onClick={() => resetCalls()}>
+                    <BiReset />
+                    Reset
+                </button>
+            </div>
             { activityData.map((activity) => {
                 return (
                     <div key={activity.id} className='card'>
@@ -40,8 +76,15 @@ const Activity = () => {
                                 <p className='text callNumber'>{activity.to}</p>
                                 <p className='text callName'>Caller: {activity.from}</p>
                             </div>
-                            <div>
+                            <div style={{ display: 'flex', gap: '.5rem', alignItems: 'center'}}>
                                 <p className='text callTime'>{ moment(activity.created_at).format('hh:mm a') }</p>
+                                <BsArchiveFill onClick={() => archiveCall(activity.id)} />
+                                <BsFillEyeFill 
+                                    onClick={() => {
+                                        localStorage.setItem('callID', activity.id)
+                                        navigate(`/${activity.id}`)
+                                    }} 
+                                />
                             </div>
                         </div>
                     </div>
