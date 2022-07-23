f8d6947ede0c437c032189a463dc137ccd3aab3c
Haris-Ali <haris.ali@vizteck.com>
2022-07-23 13:57:25 +0500

    activity component listing

--- src/apis/index.js
+++ src/apis/index.js
@@ -6,10 +6,11 @@ const serverURL = process.env.REACT_APP_API_URL
 const getRequest = async (endPoint) => {
     try {
         const data = await axios.get(`${serverURL}${endPoint}`)
-        console.log(data)
         return data
     } catch (error) {
         console.log(error)
         return error
     }
 }
+
+export { getRequest }
--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getRequest } from '../apis'
+import { ENDPOINTS } from '../apis/endpoints'
+import { BsFillTelephoneInboundFill, BsFillTelephoneOutboundFill } from "react-icons/bs";
+import moment from 'moment'
+import './Activity.css'
 
 const Activity = () => {
-  return (
-    <div>Activity</div>
-  )
+    const [activityData, setActivityData] = useState([])
+    
+    useEffect(() => {
+        getActivityList()
+    }, [])
+
+    async function getActivityList() {
+        try {
+            const URL = ENDPOINTS.getActivities
+            const { data, status } = await getRequest(URL)
+            // console.log(data, status)
+            if (status === 200) setActivityData(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+
+    return (
+        <div>
+            { activityData.map((activity) => {
+                return (
+                    <div key={activity.id} className='card'>
+                        { activity.direction === 'inbound' ? (
+                            <BsFillTelephoneInboundFill />
+                        ) : (
+                            <BsFillTelephoneOutboundFill />
+                        )}
+                        <div className='details'>
+                            <div>
+                                <p className='text callNumber'>{activity.to}</p>
+                                <p className='text callName'>Caller: {activity.from}</p>
+                            </div>
+                            <div>
+                                <p>{ moment(activity.created_at).format('HH:mm a') }</p>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }) }
+        </div>
+    )
 }
 
 export default Activity
